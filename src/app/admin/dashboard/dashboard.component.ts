75131f0725de0e52635e0b1a8b293b842e324c55
Chairul Sabri <chairulsabri734@gmail.com>
2022-11-11 16:56:28 +0700

    modules & Service

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DashboardService } from 'src/app/dashboard.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -16,18 +16,17 @@ export class DashboardComponent implements OnInit
   ProjectCost: number = 0;
   CurrentExpenditure: number = 0;
   AvailableFunds: number = 0;
-  
+  todayDate : Date = new Date();
+
   Clients: string[] = [];
   Projects: string[] = [];
   Years: number[] = [];
   TeamMembersSummary: any = [];
   TeamMembers: any = [];
 
-
-  constructor(
-   
-  ) { 
-   
+  constructor(private dashboardService: DashboardService) 
+  { 
+    
   }
 
 
@@ -41,7 +40,7 @@ export class DashboardComponent implements OnInit
     this.ProjectCost = 2113507;
     this.CurrentExpenditure = 96788;
     this.AvailableFunds = 52536;
-
+    this.todayDate = new Date;
     this.Clients = [
       "PT. Sumatera Sarana", "Frontend", "Kota Medan"
     ];
@@ -50,34 +49,13 @@ export class DashboardComponent implements OnInit
       "Backend", "Frontend", "UI.UX Designer", "Analis"
     ];
     console.log(this.Projects)
-    for (var i = 2019; i >= 2010; i--)
+    for (var i = 2022; i >= 2010; i--)
       {
         this.Years.push(i);
       }
       console.log(this.Years)
+      this.TeamMembersSummary = this.dashboardService.getTeamMembersSummary();
 
-       this.TeamMembersSummary = [
-      {
-        Region: 'East',
-        TeamMembersCount: 20,
-        TemporarilyUnavailableMembers: 4,
-      },
-      {
-        Region: 'West',
-        TeamMembersCount: 15,
-        TemporarilyUnavailableMembers: 8,
-      },
-      {
-        Region: 'South',
-        TeamMembersCount: 17,
-        TemporarilyUnavailableMembers: 1,
-      },
-      {
-        Region: 'North',
-        TeamMembersCount: 15,
-        TemporarilyUnavailableMembers: 6,
-      },
-    ];
 
     this.TeamMembers= [
       {
@@ -112,6 +90,31 @@ export class DashboardComponent implements OnInit
           {ID: 16, Name: "Ford", Status: "Busy"}
         ]
       }
-    ]
+    ];
+  }
+
+  onProjectChange($event: any)
+  {
+    if ($event.target.innerHTML.trim() == 'Backend')
+    {
+      this.ProjectCost = 2113507;
+      this.CurrentExpenditure = 96788;
+      this.AvailableFunds = 52436;
+    } else if ($event.target.innerHTML.trim() == 'Frontend')
+    {
+      this.ProjectCost = 88923;
+      this.CurrentExpenditure = 22450;
+      this.AvailableFunds = 2640;
+    } else if ($event.target.innerHTML.trim() == 'UI.UX Designer')
+    {
+      this.ProjectCost = 662183;
+      this.CurrentExpenditure = 7721;
+      this.AvailableFunds = 9811;
+    } else if ($event.target.innerHTML.trim() == 'Analis')
+    {
+      this.ProjectCost = 928431;
+      this.CurrentExpenditure = 562;
+      this.AvailableFunds = 883;
+    }
   }
 }
